3bd59d3932bd3ae8c70c67bb86fb89a9d36cf4fb
jerryluo <luojianwei@qiancangkeji.cn>
2019-09-27 13:57:25 +0800

    gc

---
+++ src/main/java/com/limao/jvm/demo/Demo1.java
@@ -0,0 +1,27 @@
+package com.limao.jvm.demo;
+/**
+ * -XX:NewSize=5242880                                 5M
+ * -XX:MaxNewSize=5242880                              5M
+ * -XX:InitialHeapSize=10485760                       10M
+ * -XX:MaxHeapSize=10485760                           10M
+ * -XX:SurvivorRatio=8
+ * -XX:PretenureSizeThreshold=10485760                10M
+ * -XX:+UseParNewGC
+ * -XX:+UseConcMarkSweepGC
+ * -XX:+PrintGCDetails
+ * -XX:+PrintGCTimeStamps
+ * -Xloggc:gc.log
+ *
+ * @author <a href="mailto:luojianwei@qiancangkeji.cn">LuoJianwei</a>
+ * @since 1.0.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        byte[] array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+    }
+}
--- src/main/java/com/limao/jvm/demo/JvmDemoApplication.java
+++ src/main/java/com/limao/jvm/demo/JvmDemoApplication.java
@@ -2,7 +2,11 @@ package com.limao.jvm.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ *
+ * @author <a href="mailto:luojianwei@qiancangkeji.cn">LuoJianwei</a>
+ * @since 1.0.0
+ */
 @SpringBootApplication
 public class JvmDemoApplication {
 
