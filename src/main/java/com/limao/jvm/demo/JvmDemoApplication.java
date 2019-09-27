e9b44b255f1545133b6fa5d4fc195cc2a3425e4b
jerryluo <luojianwei@qiancangkeji.cn>
2019-09-27 10:57:27 +0800

    initialize

---
+++ src/main/java/com/limao/jvm/demo/JvmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.limao.jvm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmDemoApplication.class, args);
+	}
+
+}
